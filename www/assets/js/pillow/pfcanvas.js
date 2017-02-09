4183adf123581216b933d897ef16b3e2708d448c
Markus Tran <thebluepillow@gmail.com>
2017-02-08 17:59:40 -0800

    fix bugs

--- www/assets/js/pillow/pfcanvas.js
+++ www/assets/js/pillow/pfcanvas.js
@@ -1,6 +1,6 @@
 /*
  *   A little tool to turn a DOM element into a canvas. Requires jQuery.
- *   
+ *
  *   Example usage:
  *
  *   Pillow.canvas(document.body, (function() {
@@ -8,13 +8,13 @@
  *       var color = "#"+("00000"+((1<<24)*Math.random()|0).toString(16)).slice(-6);
  *       var size = 200;
  *       var theta = 0;
- *   
+ *
  *       // Loop code
  *       return function(ctx) {
  *           var width = ctx.canvas.width, height = ctx.canvas.height;
  *           var cX = width/2, cY = height/2;
  *           ctx.clearRect(0, 0, width, height);
- *   
+ *
  *           ctx.fillStyle = color;
  *           ctx.beginPath();
  *           ctx.moveTo(cX + size*Math.cos(theta), cY + size*Math.sin(theta));
@@ -22,7 +22,7 @@
  *           ctx.lineTo(cX + size*Math.cos(theta + Math.PI*4/3), cY + size*Math.sin(theta + Math.PI*4/3));
  *           ctx.closePath();
  *           ctx.fill();
- *   
+ *
  *           theta += .03;
  *       };
  *   })(), {
@@ -90,5 +90,12 @@
 		});
 
 	};
+
+// Fix for things using $(...).pillowcanvas()
+$.fn.extend({
+  pillowcanvas: function(draw, options) {
+    return Pillow.canvas(this, draw, options);
+  }
+});
 
 })(window.Pillow = window.Pillow || {}, jQuery);
