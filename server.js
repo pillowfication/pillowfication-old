8d6afcb8f2bd45d75c1b3f4877eaf10cb3f43f4f
Markus Tran <thebluepillow@gmail.com>
2017-11-10 00:28:56 -0800

    serve-favicon

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const express = require('express')
+const favicon = require('serve-favicon')
 const app = express()
 const port = process.argv[2] || process.env.PFN_OLD_PORT || 80
 
+app.use(favicon(path.join(__dirname, 'www', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'www')))
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'www', 'error_pages', '404.html'))
