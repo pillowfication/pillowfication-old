f80e12681f9b2d64701624d5f4bd33b5faa7d3b2
Markus Tran <thebluepillow@gmail.com>
2017-12-09 20:45:43 -0800

    update package.json

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
 const favicon = require('serve-favicon')
+
 const app = express()
 const port = process.argv[2] || process.env.PFN_OLD_PORT || 80
 
-app.use(favicon(path.join(__dirname, 'www', 'favicon.ico')))
-app.use(express.static(path.join(__dirname, 'www')))
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'www', 'error_pages', '404.html'))
+app.use(favicon(path.resolve(__dirname, 'www/favicon.ico')))
+app.use(express.static(path.resolve(__dirname, 'www')))
+app.use((_, response) => {
+  response
+    .status(404)
+    .sendFile(path.resolve(__dirname, 'www/error_pages/404.html'))
 })
 
 app.listen(port, () => {
-  console.log(`App started on port ${port}`)
+  console.log(`pillowfication-old started on port ${port}`)
 })
